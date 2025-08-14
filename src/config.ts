7f2a06ed69cad513babe067e6671d932fcc014a4
sosel <web@mailberry.com.cn>
2025-08-14 12:01:48 +0800

    Update config.ts
    waline change Twikoo

--- src/config.ts
+++ src/config.ts
@@ -96,12 +96,12 @@ export default {
   Comment: {
     // Twikoo 评论
     Twikoo: {
-      enable: false,
+      enable: true,
       envId: ''
     },
     // Waline 评论
     Waline: {
-      enable: true,
+      enable: false,
       serverURL: ''
     }
   },
